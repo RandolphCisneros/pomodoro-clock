a12a9b89f4ff7bc332a98ea3fcb33a19f60ce7ac
Randolph Cisneros <cisnerosRandolph@gmail.com>
2020-02-29 12:42:59 -0500

    pomodoro added style and script stuff

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 let screenContainer = document.getElementById("screenContainer");
 
+let firstClockInitailzed = false;
+
+const startingSeconds = 0;
+const rowDivClassName = "rowDiv";
+const workingMinutes = 25;
+const restingMinutes = 5;
+
 initializeInnerContainers();
+
 function initializeInnerContainers() {
-    intializeTwentyFiveClockContainer();
-    initializeFiveClockContainer();
+    initializeClockContainers();
+}
+
+function initializeClockContainers() {
+    initializeClockContainer(workingMinutes);
+    initializeClockContainer(restingMinutes);
+}
+
+function initializeClockContainer(minutes) {
+    let rowDiv = document.createElement('div');
+    rowDiv.className = rowDivClassName;
+    let form = document.createElement('form');
+    form.textContent = minutes + " : " + startingSeconds;
+    rowDiv.appendChild(form);
+    screenContainer.appendChild(rowDiv);
+    initializeClockRow(minutes);
+}
+
+function initializeClockRow(minutes) {
+    initializeTimeContainer(minutes);
+    initializeTimeContainer(startingSeconds);
+}
+
+function initializeTimeContainer(time) {
+
 }
